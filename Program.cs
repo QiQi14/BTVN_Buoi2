048ab0449bca72eacc732bc07cb0aa9b949493b2
LeonPham99 <giahuypham1899@gmail.com>
2023-09-07 14:29:44 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,134 @@
 namespace BTVN;
 class Buoi2
 {
+
+    public static void Main(string[] args)
+    {
+        int luachon = 2;
+        Console.WriteLine("Bai Tap Can Kiem Tra");
+        Console.WriteLine("Bai tap 1 ban nhap so 1 ");
+        Console.WriteLine("Bai tap 2 ban nhap so 2 ");
+        Console.Write("Moi Ban Nhap Bai Tap So = ");
+        luachon = int.Parse(Console.ReadLine());
+        switch (luachon)
+        {
+            case 1:
+                BaiTap1();
+                break;
+            case 2:
+                BaiTap2();
+                break;
+            default:
+                Console.WriteLine("Ban Nhap Lua chon khong co bai tap");
+                break;
+        }
+    }
+    public static void NhapMang(int[] a, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("a[" + i + "]=");
+            a[i] = int.Parse(Console.ReadLine());
+        }
+    }
+
+    public static void XuatMang(int[] a, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a[i] + " ");
+        }
+    }
+
+    public static void BaiTap1()
+    {
+        Console.Write("Nhap vao mang n phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        NhapMang(a, n);
+
+        Console.Write("Phan Tu Giong Nhau la : ");
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (a[i] == a[j])
+                {
+                    Console.Write(a[i] + " ");
+                }
+            }
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Phan Tu Khac Nhau la : ");
+        Console.Write("Co Mang ");
+        XuatMang(a, n);
+        Console.Write("Thi ");
+        for (int i = 0; i < a.Length; i++)
+        {
+            bool phantuTrung = true;
+
+            for (int j = 0; j < a.Length; j++)
+            {
+                if (i != j && a[i] == a[j])
+                {
+                    phantuTrung = false;
+                    break;
+                }
+            }
+            if (phantuTrung)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+        Console.Write("la phan tu khong bi trung");
+        Console.ReadKey();
+    }
+
+    public static void NhapMangbt2(string[] tenhs, double[] diemhs, int soluong)
+    {
+        for (int i = 0; i < soluong; i++)
+        {
+            Console.Write("Nhap ten hoc sinh thu " + (i + 1) + " = ");
+            tenhs[i] = Console.ReadLine();
+
+            Console.Write("Nhap diem hoc sinh " + tenhs[i] + " = ");
+            diemhs[i] = Double.Parse(Console.ReadLine());
+        }
+    }
+
+    public static void XuatMangbt2(string[] tenhs, double[] diemhs, int soluong)
+    {
+        Console.WriteLine("Danh Sach Sinh Vien va Diem: ");
+
+        for (int i = 0; i < soluong; i++)
+        {
+            Console.WriteLine("Hoc Sinh Thu " + (i + 1) + " Ten: " + tenhs[i] + " Diem: " + diemhs[i]);
+        }
+    }
+
+    public static void BaiTap2()
+    {
+        Console.Write("Nhap so luong hoc sinh lop hoc = ");
+        int soluong = int.Parse(Console.ReadLine());
+        string[] tenhs = new string[soluong];
+        double[] diemhs = new double[soluong];
+        NhapMangbt2(tenhs, diemhs, soluong);
+        int max = 0;
+        for (int i = 1; i < soluong; i++)
+        {
+            if (diemhs[i] > diemhs[max])
+            {
+                max = i;
+
+            }
+        }
+        Console.WriteLine("Hoc Vien " + tenhs[max] + " Co Diem Cao Nhat la = " + tenhs[max] + " - " + diemhs[max]);
+        Console.ReadKey();
+        //XuatMangbt2(tenhs, diemhs, soluong);
+    }
+
+    /*
     public static void Main()
     {
         int[] test1 = { 1, 2, 3, 3, 1};
@@ -38,4 +166,5 @@ class Buoi2
         string result = "Khong biet";
         Console.WriteLine("Nguoi co diem so cao nhat la: " + result);
     }
+    */
 }
