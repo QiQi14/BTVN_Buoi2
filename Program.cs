0084d25e902f8e2412f1761b8fe3779252a56c5d
dinhnha1402 <dinhnha1402@gmail.com>
2023-09-07 22:39:33 +0700

    add_test_case

--- Program.cs
+++ Program.cs
@@ -9,13 +9,16 @@ class Buoi2
 {
     public static void Main()
     {
-        int[] test1 = { 1, 2, 3, 3, 1};
-        int[] test2 = { 1, 1, 1, 1, 1, 6};
-        int[] test3 = { 1, 2, 3, 4, 3, 2, 1};
-        int[] test4 = { 1, 3, 3, 3, 1, 2, 5, 2};
-        string[] name = { "A", "B", "C", "D", "E", "F"};
-        int[] grade = { 5, 4, 5, 7, 4, 6};
-        
+        int[] test1 = { 1, 2, 3, 3, 1 };
+        int[] test2 = { 1, 1, 1, 1, 1, 6 };
+        int[] test3 = { 1, 2, 3, 4, 3, 2, 1 };
+        int[] test4 = { 1, 3, 3, 3, 1, 2, 5, 2 };
+        string[] test_name_01 = { "A", "B", "C", "D", "E", "F"};
+        int[] test_grade_01 = { 5, 4, 5, 7, 4, 6 };
+        string[] test_name_02 = { "A", "B", "C", "D", "E", "F" ,"G", "H"};
+        int[] test_grade_02 = { 2, 6, 5, 7, 8, 9, 9, 9};
+        string[] test_name_03 = { "A", "B"};
+        int[] test_grade_03 = {};
 
 
         System.Console.WriteLine("BTVN Buoi 2:");
@@ -25,7 +28,12 @@ class Buoi2
         System.Console.WriteLine("test case 3: " + Bai1(test3));
         System.Console.WriteLine("test case 4: " + Bai1(test4));
         System.Console.WriteLine("Ket qua bai 2:");
-        Bai2(name, grade);
+        System.Console.WriteLine("test case 1: "); 
+        Bai2(test_name_01, test_grade_01);
+        System.Console.WriteLine("test case 2: ");
+        Bai2(test_name_02, test_grade_02);
+        System.Console.WriteLine("test case 3: ");
+        Bai2(test_name_03, test_grade_03);
     }
 
     private static string Bai1(int[] input)
@@ -37,22 +45,20 @@ class Buoi2
 
         //Xử lý kết quả dựa theo đầu vào input rồi gán vào biến result
         int result = -1;
-        
+
         for (int i = 0; i < input.Length; i++)
         {
-            int flag = 0;
+            bool flag = true;
             for (int j = 0; j < input.Length; j++)
             {
-                if (flag == 2)
+
+                if (i != j && input[i] == input[j])
                 {
+                    flag = false;
                     break;
                 }
-                else if (input[i] == input[j])
-                {
-                    flag = flag + 1;
-                }
             }
-            if (flag == 1)
+            if (flag)
             {
                 result = input[i];
                 break;
@@ -67,9 +73,9 @@ class Buoi2
             return "Phan tu khong trung trong mang la " + result;
     }
 
-    private static void Bai2(string[]name, int[] grade)
+    private static void Bai2(string[] name, int[] grade)
     {
-        if ((grade == null) || (name == null))
+        if ((grade.Length == 0) || (name.Length == 0))
         {
             Console.WriteLine("Mang rong");
             return;
@@ -77,8 +83,8 @@ class Buoi2
 
         //Xử lý kết quả dựa theo đầu vào là mảng name và mảng grade rồi gán vào biến result
         int max = grade.Max();
-        List<int> list_max = new List<int> ();
-        for (int i = 0;i < grade.Length;i++)
+        List<int> list_max = new List<int>();
+        for (int i = 0; i < grade.Length; i++)
         {
             if (grade[i] == max)
             {
