6ea099f62ee4d548c6598d07fc79bbf0314fddf3
ToanNguyen995 <toan03031995@gmail.com>
2023-09-09 16:33:20 +0700

    commit bài tập 1, 2

---
+++ PhầnTử.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class KiemtraPhanTu
+{
+    static void nhap(int[] a, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("a[" + i + "]= ");
+            a[i] = int.Parse(Console.ReadLine());
+        }
+    }
+    static void xuat(int[] a, int n)
+    {
+        Console.Write("co mang:");
+        for (int i = 0; i < n; i++)
+            Console.Write(a[i] + " ");
+    }
+    static void xuatkotrung(int[] a, int n)
+    {
+        int i, j;
+        Console.Write("Cac phan tu khong trung lap: ");
+        for (i = 0; i < a.Length; i++)
+        {
+            for (j = 0; j < a.Length; j++)
+            {
+                if (i == j)
+                    continue;
+                if (a[j] == a[i])
+                    break;
+            }
+            if (a.Length == j)
+            {
+                Console.Write(a[i] + " ");
+
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap so phan tu cua mang: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[100];
+        nhap(a, n);
+        xuat(a, n);
+        xuatkotrung(a, n);
+        Console.ReadKey();
+    }
+}
